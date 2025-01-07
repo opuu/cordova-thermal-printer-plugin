92b9d87d6c9291e2ab0160a05f9c9703a228e591
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2025-01-07 16:42:52 +0100

    Update ThermalPrinterCordovaPlugin.java

--- src/android/ThermalPrinterCordovaPlugin.java
+++ src/android/ThermalPrinterCordovaPlugin.java
@@ -297,7 +297,7 @@ public class ThermalPrinterCordovaPlugin extends CordovaPlugin {
 
                 int width = decodedByte.getWidth(), height = decodedByte.getHeight();
 
-                int maxSize = 820;
+                int maxSize = 800;
                 int outWidth;
                 int outHeight;
                 if(width > height){
